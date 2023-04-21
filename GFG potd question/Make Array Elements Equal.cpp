38de423ae753bbb44f99126442408468fbe8df00
Ayush kumar <87802245+Ayush92347@users.noreply.github.com>
2023-04-21 11:55:20 +0530

    Create Make Array Elements Equal.cpp

---
+++ GFG potd question/Make Array Elements Equal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+
+  public:
+
+    long long int minOperations(int N) {
+
+        long long divide = N / 2; 
+
+        long long rem = N - divide; 
+
+        return divide * rem; 
+
+    }
+
+};
