e504d68b29f45a472ca38e157c0ee83bd2ac1b7d
Ayush kumar <87802245+Ayush92347@users.noreply.github.com>
2023-04-23 16:35:39 +0530

    Create Absolute List Sorting.cpp

---
+++ GFG potd question/Absolute List Sorting.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+    Node* sortList(Node* head)
+    {
+        // Initialize previous and current nodes
+        Node* prev = head;
+        Node* curr = head->next;
+
+        // Traverse list
+        while (curr != NULL)
+        {
+            // If curr is smaller than prev, then
+            // it must be moved to head
+            if (curr->data < prev->data)
+            {
+                // Detach curr from linked list
+                prev->next = curr->next;
+
+                // Move current node to beginning
+                curr->next = head;
+                head = curr;
+
+                // Update current
+                curr = prev;
+            }
+
+            // Nothing to do if current element
+            // is at right place
+            else
+                prev = curr;
+
+            // Move current
+            curr = curr->next;
+        }
+        
+        return head;
+    }
+};
